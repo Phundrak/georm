003af7110788afb31850a08b023cdb5b3f4fd9b5
Lucien Cartier-Tilet <lucien@phundrak.com>
2025-01-31 22:58:32 +0100

    chore: simplify flake.nix and update flake.lock

--- flake.nix
+++ flake.nix
@@ -13,27 +13,7 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
         rustVersion = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustVersion;
-          rustc = rustVersion;
-        };
-
-        libName = "georm";
-
-        libRustBuildGeorm = rustPlatform.buildRustPackage {
-          pname = libName;
-          version = "0.1.0";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-          buildPhase = ''
-SQLX_OFFLINE="1" cargo build --release
-'';
-        };
       in {
-        packages = {
-          lib = libRustBuildGeorm;
-        };
-        defaultPackage = libRustBuildGeorm;
         devShell = with pkgs; mkShell {
           buildInputs = [
             bacon
